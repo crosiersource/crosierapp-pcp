c97ccdde135eb6370cc5719109d5c0141698c877
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2019-05-07 16:38:08 -0300

    APP.sql

--- sql/APP.sql
+++ sql/APP.sql
@@ -16,10 +16,10 @@ DELETE FROM cfg_entmenu WHERE uuid = 'f9e6121f-d696-49dc-90c4-110b5d15b891';
 
 
 INSERT INTO cfg_app(uuid,nome,obs,default_entmenu_uuid,inserted,updated,estabelecimento_id,user_inserted_id,user_updated_id) 
-VALUES ('bfacf76d-2ff2-42c2-854c-bb5b10d78c92','PCP','CrosierApp para Controle de Produção','90a155bf-0d77-499c-8465-330bc4ce1dd9',now(),now(),1,1,1);
+VALUES ('bfacf76d-2ff2-42c2-854c-bb5b10d78c92','crosierapp-pcp','CrosierApp para Controle de Produção','90a155bf-0d77-499c-8465-330bc4ce1dd9',now(),now(),1,1,1);
 
 INSERT INTO cfg_program(uuid, descricao, url, app_uuid, entmenu_uuid ,inserted, updated, estabelecimento_id, user_inserted_id, user_updated_id)
-VALUES ('5875756c-d16d-4906-b4d0-fd60db47ba21','Dashboard - PCP', '/', 'bfacf76d-2ff2-42c2-854c-bb5b10d78c92', null, now(), now(), 1, 1, 1);
+VALUES ('5875756c-d16d-4906-b4d0-fd60db47ba21','Dashboard - crosierapp-pcp', '/', 'bfacf76d-2ff2-42c2-854c-bb5b10d78c92', null, now(), now(), 1, 1, 1);
 
 
 
@@ -29,7 +29,7 @@ VALUES ('0cfdd630-4382-4a6a-beed-a0e5ec73f8df','PCP','fas fa-columns','CROSIERCO
 
 -- Entrada de menu raíz para este CrosierApp (NÃO É EXIBIDO)
 INSERT INTO cfg_entmenu(uuid,label,icon,tipo,program_uuid,pai_uuid,ordem,css_style,inserted,updated,estabelecimento_id,user_inserted_id,user_updated_id)
-VALUES ('90a155bf-0d77-499c-8465-330bc4ce1dd9','PCP - MainMenu','','PAI','',null,0,null,now(),now(),1,1,1);
+VALUES ('90a155bf-0d77-499c-8465-330bc4ce1dd9','crosierapp-pcp (MainMenu)','','PAI','',null,0,null,now(),now(),1,1,1);
 
 -- Entrada de menu para o menu raíz deste CrosierApp com apontamento para o Dashboard deste CrosierApp TAMBÉM! (É EXIBIDO COMO PRIMEIRO ITEM DO MENU DESTE CROSIERAPP)
 INSERT INTO cfg_entmenu(uuid,label,icon,tipo,program_uuid,pai_uuid,ordem,css_style,inserted,updated,estabelecimento_id,user_inserted_id,user_updated_id)
