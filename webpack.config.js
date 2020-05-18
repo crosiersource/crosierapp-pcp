7ae7cf1070955888eb4132d0280fd2e3fb8964f9
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2020-05-18 19:30:29 -0300

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
 var Encore = require('@symfony/webpack-encore');
 
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
 const webpack = require('webpack');
 
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 Encore
-// directory where compiled assets will be stored
+    // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
-
-    // fixes modules that expect jQuery to be global
     .autoProvidejQuery()
     .addPlugin(new CopyWebpackPlugin([
         // copies to {output}/static
         {from: './assets/static', to: 'static'}
     ]))
-    .enableSassLoader()
+    // o summmernote tem esta dependência, mas não é necessária
+    .addPlugin(new webpack.IgnorePlugin(/^codemirror$/))
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
     /*
      * ENTRY CONFIG
      *
@@ -26,9 +32,8 @@ Encore
      * (including one that's included on every page - e.g. "app")
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    // .createSharedEntry('bse_layout', './assets/js/bse/layout.js')
     .addEntry('tipoInsumoList', './assets/js/tipoInsumoList.js')
     .addEntry('tipoArtigoList', './assets/js/tipoArtigoList.js')
     .addEntry('insumoList', './assets/js/insumoList.js')
@@ -38,6 +43,14 @@ Encore
     .addEntry('fichaTecnica', './assets/js/fichaTecnica.js')
     .addEntry('fichaTecnica_clonar', './assets/js/fichaTecnica_clonar.js')
     .addEntry('fichaTecnicaItemForm', './assets/js/fichaTecnicaItemForm.js')
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    // .enableSingleRuntimeChunk()
+    .disableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
@@ -51,12 +64,12 @@ Encore
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
-    .configureBabel(() => {
-    }, {
-        useBuiltIns: 'usage',
-        corejs: 3
+
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
     })
-    .enableSingleRuntimeChunk()
 
 // enables Sass/SCSS support
 //.enableSassLoader()
@@ -64,8 +77,17 @@ Encore
 // uncomment if you use TypeScript
 //.enableTypeScriptLoader()
 
+// uncomment to get integrity="..." attributes on your script & link tags
+// requires WebpackEncoreBundle 1.4 or higher
+//.enableIntegrityHashes(Encore.isProduction())
+
 // uncomment if you're having problems with a jQuery plugin
 //.autoProvidejQuery()
+
+// uncomment if you use API Platform Admin (composer req api-admin)
+//.enableReactPreset()
+//.addEntry('admin', './assets/js/admin.js')
 ;
 
 module.exports = Encore.getWebpackConfig();
+
