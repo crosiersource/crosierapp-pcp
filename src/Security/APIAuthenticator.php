99b089f1d6e8f7872287e88a187dfb9b9ad6c7a8
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-04-10 12:36:10 -0300

    ...

--- src/Security/APIAuthenticator.php
+++ src/Security/APIAuthenticator.php
@@ -3,14 +3,18 @@
 namespace App\Security;
 
 use CrosierSource\CrosierLibBaseBundle\Security\APIAuthenticatorTrait;
-use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 
 /**
  * Autenticação por API Token.
  *
- * @package App\Security
+ * @author Carlos Eduardo Pauluk
  */
-class APIAuthenticator extends AbstractGuardAuthenticator
+class APIAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
 {
 
     use APIAuthenticatorTrait;
