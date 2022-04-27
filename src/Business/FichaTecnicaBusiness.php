7b4f6bfb2b7c0a0a129c5363a4ec14d6955f3073
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2022-04-27 16:34:49 -0300

    ...

--- src/Business/FichaTecnicaBusiness.php
+++ src/Business/FichaTecnicaBusiness.php
@@ -31,7 +31,7 @@ class FichaTecnicaBusiness
     private EntityManagerInterface $doctrine;
 
     private CalculoPreco $calculoPreco;
-
+    
     /**
      * @required
      * @param CalculoPreco $calculoPreco
